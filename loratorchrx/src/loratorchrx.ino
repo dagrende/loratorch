edbd1c243b941ef637bd55f39377ba2e8c68a447
dagrende <dag.rende@find-out.se>
2017-08-13 13:51:50 +0200

    new schematic and rx driver

--- loratorchrx/src/loratorchrx.ino
+++ loratorchrx/src/loratorchrx.ino
@@ -11,10 +11,11 @@
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
 #define LAMP_SENSE_PIN D1
+#define LAMP_OUT_PIN D0
 
 RH_RF95 driver(D8, D2); //
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
-Button lampOnButton(LAMP_SENSE_PIN, false, false, 50);
+// Button lampOnButton(LAMP_SENSE_PIN, false, false, 50);
 
 void setup() {
   WiFi.disconnect();
@@ -22,8 +23,8 @@ void setup() {
   WiFi.forceSleepBegin();
 
   // setup torch lamp control pin 1=lit 0=unlit
-  pinMode(D4, OUTPUT);
-  digitalWrite(D4, 0);
+  pinMode(LAMP_OUT_PIN, OUTPUT);
+  digitalWrite(LAMP_OUT_PIN, 0);
 
   // setup lamp on sense input pin
 //  pinMode(LAMP_SENSE_PIN, INPUT);
@@ -41,13 +42,13 @@ void setup() {
   driver.setFrequency(868.0); // Swedish lora frequency
 }
 
-uint8_t data[] = "And hello back to you";
 // Dont put this on the stack:
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 float prevVoltage = 0;
 
 void loop() {
   if (manager.available()) {
+    Serial.print(".");
     // Wait for a message addressed to us from the client
     uint8_t len = sizeof(buf);
     uint8_t from;
@@ -58,26 +59,26 @@ void loop() {
       buf[len] = 0;
       Serial.println((char*)buf);
       if (strcmp((char *)buf, "on") == 0) {
-        digitalWrite(D4, 0);
+        digitalWrite(LAMP_OUT_PIN, 0);
       } else if (strcmp((char *)buf, "off") == 0) {
-        digitalWrite(D4, 1);
+        digitalWrite(LAMP_OUT_PIN, 1);
       }
     }
   }
 
-  // detect lamp on button (only valid when lamp is remote controlled off - that is D4 set to 1)
-  lampOnButton.read();
-  if (lampOnButton.wasPressed()) {
-    Serial.println("turn on");
-  } else if (lampOnButton.wasReleased()) {
-    Serial.println("turn off");
-  }
+  // // detect lamp on button (only valid when lamp is remote controlled off - that is LAMP_OUT_PIN set to 1)
+  // lampOnButton.read();
+  // if (lampOnButton.wasPressed()) {
+  //   Serial.println("turn on");
+  // } else if (lampOnButton.wasReleased()) {
+  //   Serial.println("turn off");
+  // }
 
-  // measure battery voltage
-  float voltage = analogRead(A0) * 6.02 / 789.0;
-  if (abs(voltage - prevVoltage) > 0.05) {
-    Serial.print(voltage);
-    Serial.println("V");
-    prevVoltage = voltage;
-  }
+  // // measure battery voltage
+  // float voltage = analogRead(A0) * 6.02 / 789.0;
+  // if (abs(voltage - prevVoltage) > 0.05) {
+  //   Serial.print(voltage);
+  //   Serial.println("V");
+  //   prevVoltage = voltage;
+  // }
 }
